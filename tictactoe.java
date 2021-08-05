baa8e2faa8f1e60bcb05c8f61cbfd71aba5278f6
Mohsin <76669754+MohammadMohsinKhan@users.noreply.github.com>
2021-08-05 08:39:27 -0700

    Added minimax to TicTacToe
    added a hard difficulty which uses the minimax algorithm so that it is impossible to beat the bot.
    changes to be included in future updates includes getting rid of redundant functions, cleaning up the mediumMove function which currently has a bunch of redundant if conditions and adding more comments.

--- tictactoe.java
+++ tictactoe.java
@@ -1,7 +1,99 @@
 import java.util.*;
-
+//up till line 52 in github done.
 public class tictactoe {
-    static void mediumMove (String matrix[][], String piece) {
+   
+    static void hardMove(String matrix[][], String piece) {
+        int[][] moves = availableMoves(matrix);
+        int[] bestMove = new int[2];
+        int score;
+        int bestScore = Integer.MIN_VALUE;
+
+        for (int[] move: moves) {
+            matrix[move[0]][move[1]] = piece;
+            score = minimax(matrix, piece, 0, false);
+            matrix[move[0]][move[1]] = " "; //resets the gameboard after the available move is tested
+            
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove[0] = move[0];
+                bestMove[1]= move[1];
+            }
+        }
+        matrix[bestMove[0]][bestMove[1]] = piece;
+        displayBoard(matrix);
+    }
+
+    static int minimax(String matrix[][], String piece, int depth, boolean isMaximizing) {
+        int[][] moves = availableMoves(matrix);
+        String result = winCheck(matrix);
+        int bestScore = Integer.MIN_VALUE;
+        int score;
+
+        String enemyPiece = piece.equalsIgnoreCase("X") ? "O" : "X";
+        
+        if (!result.equalsIgnoreCase("")) { //checks if the board has reached a terminal state ie the game has ended and then returns a score
+            if (result.equalsIgnoreCase(piece)) {
+                return 10;
+            } else if (result.equalsIgnoreCase(enemyPiece)) {
+                return -10;
+            } else {
+                return 0;
+            }
+        }
+
+        if(isMaximizing) { // it is the AI's turn and it wants to maximize its score by picking the optimal move
+            bestScore = Integer.MIN_VALUE;
+            for (int[] move: moves) {
+                matrix[move[0]][move[1]] = piece;
+                score = minimax(matrix, piece, depth + 1, false);
+                matrix[move[0]][move[1]] = " ";
+                bestScore = Math.max(score, bestScore);
+            }
+            return bestScore;
+        } else { // it is the human/enemy bot's tuen and they want to pick a move optimal for themselves so they will minimize the score.
+            bestScore = Integer.MAX_VALUE;
+            for (int[] move: moves) {
+                matrix[move[0]][move[1]] = enemyPiece;
+                score = minimax(matrix, piece, depth + 1, true);
+                matrix[move[0]][move[1]] = " ";
+                bestScore = Math.min(score, bestScore);
+            }
+            return bestScore;
+        }
+
+    }
+
+    static int emptyCount(String matrix[][]) { //returns a count of how many empty cells remain on the game board
+        int emptyCount = 0;
+        
+        for (int i = 1; i < matrix.length - 1; i++) {
+            for (int j = 1; j < matrix.length - 1; j++) {
+                if (matrix[i][j].equalsIgnoreCase(" ")) {
+                    ++emptyCount;
+                }
+            }
+        }
+        return emptyCount;
+    }
+
+    static int[][] availableMoves(String matrix[][]) { // returns a list of available moves left on the board 
+        
+        int[][] moves = new int[emptyCount(matrix)][2];
+        int ind = 0;
+
+        for (int i = 1; i < matrix.length - 1; i++) {
+            for (int j = 1; j < matrix.length - 1; j++) {
+                if (matrix[i][j].equalsIgnoreCase(" ")) {
+                    moves[ind][0] = i;
+                    moves[ind][1] = j;
+                    ++ind;
+                }
+            }
+        }
+        return moves;
+    }
+
+    static void mediumMove(String matrix[][], String piece) {
         int counter = 0;
         int mark = 0;
         boolean empty = false;
@@ -177,7 +269,8 @@ public class tictactoe {
     }
 
     static void easyMove (String matrix[][], String piece) {
-        while (true) {
+        // generates a random move
+        while (true) { 
             Random random = new Random();
             int row = random.nextInt(3) + 1;
             int col = random.nextInt(3) + 1;
@@ -193,6 +286,7 @@ public class tictactoe {
         while (true) {
             System.out.print("Enter the coordinates: ");
             Scanner user_coord = new Scanner(System.in);
+            // checks to see whether the coordinates entered are in the correct format and within the range
             try {
                 int r = user_coord.nextInt();
                 int c = user_coord.nextInt();
@@ -213,7 +307,7 @@ public class tictactoe {
         
     }
 
-    static boolean fillCheck(String matrix[][]) {
+    static boolean fillCheck(String matrix[][]) { // returns a boolean of whether the board is filled
         int emptyCount = 0;
         for (int i = 1; i < matrix.length - 1;i++) {
             for (int j = 1; j < matrix[i].length - 1; j++) {
@@ -225,33 +319,29 @@ public class tictactoe {
         return emptyCount == 0;
     }
 
-    static boolean winCheck(String matrix[][]) {
+    static String winCheck(String matrix[][]) { // returns the winning piece
         boolean X = false;
         Boolean O = false;
 
         for (int i = 1; i < matrix.length - 1; i++) {
             if (matrix[i][1].equalsIgnoreCase("X") && matrix[i][2].equalsIgnoreCase("X") && matrix[i][3].equalsIgnoreCase("X")) {
-                System.out.println("X wins");
                 X = true;
-                return true;
+                return "X";
                 
             } else if (matrix[i][1].equalsIgnoreCase("O") && matrix[i][2].equalsIgnoreCase("O") && matrix[i][3].equalsIgnoreCase("O")) {
-                System.out.println("O wins");
                 O = true;
-                return true;
+                return "O";
                 
             }
 
             if (!X && !O) {
                 if (matrix[1][i].equalsIgnoreCase("X") && matrix[2][i].equalsIgnoreCase("X") && matrix[3][i].equalsIgnoreCase("X")) {
-                    System.out.println("X wins");
                     X = true;
-                    return true;
+                    return "X";
                     
                 } else if (matrix[1][i].equalsIgnoreCase("O") && matrix[2][i].equalsIgnoreCase("O") && matrix[3][i].equalsIgnoreCase("O")) {
-                    System.out.println("O wins");
                     O = true;
-                    return true;
+                    return "O";
                     
                 }
             }
@@ -259,33 +349,32 @@ public class tictactoe {
 
         if (!X && !O) {
                 if (matrix[1][1].equalsIgnoreCase("X") && matrix[2][2].equalsIgnoreCase("X") && matrix[3][3].equalsIgnoreCase("X")) {
-                    System.out.println("X wins");
                     X = true;
-                    return true;
+                    return "X";
                 } else if (matrix[1][1].equalsIgnoreCase("O") && matrix[2][2].equalsIgnoreCase("O") && matrix[3][3].equalsIgnoreCase("O")) {
-                    System.out.println("O wins");
                     O = true;
-                    return true;
+                    return "O";
                 }
         }
 
         if (!X && !O) {
             if (matrix[1][3].equalsIgnoreCase("X") && matrix[2][2].equalsIgnoreCase("X") && matrix[3][1].equalsIgnoreCase("X")) {
-                System.out.println("X wins");
                 X = true;
-                return true;
+                return "X";
             } else if (matrix[1][3].equalsIgnoreCase("O") && matrix[2][2].equalsIgnoreCase("O") && matrix[3][1].equalsIgnoreCase("O")) {
-                System.out.println("O wins");
                 O = true;
-                return true;
+                return "O";
             }
         }   
-    
-        return false;
         
+        if (fillCheck(matrix) == true) {
+            return "tie";
+        }
+
+        return "";
     }
 
-    static void displayBoard(String matrix[][]) {
+    static void displayBoard(String matrix[][]) { // function for rendering the gameboard on the terminal
         for (int i = 0; i < matrix.length;i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (i > 0 && i <= matrix.length - 2) {
@@ -298,7 +387,7 @@ public class tictactoe {
         }
     }
     public static void main(String[] args) {
-        String[] goodParameter = {"easy", "user", "medium"};
+        String[] goodParameter = {"easy", "user", "medium", "hard"};
 
         while (true) {
             String[][] board = {
@@ -328,7 +417,8 @@ public class tictactoe {
                     if (parameter[1].equalsIgnoreCase("user")) {
                         playerMove(board, num % 2 == 0 ? "O" : "X");
                         ++num;
-                        if (winCheck(board)) {
+                        if (winCheck(board).equalsIgnoreCase("X")) {
+                            System.out.println("X wins");
                             break;
                         }
                         if (fillCheck(board)) {
@@ -339,7 +429,20 @@ public class tictactoe {
                         System.out.println("Making move level \"easy\"");
                         easyMove(board, num % 2 == 0 ? "O" : "X");
                         ++num;
-                        if (winCheck(board)) {
+                        if (winCheck(board).equalsIgnoreCase("X")) {
+                            System.out.println("X wins");
+                            break;
+                        }
+                        if (fillCheck(board)) {
+                            System.out.println("Draw");
+                            break;
+                        }
+                    } else if (parameter[1].equalsIgnoreCase("medium")){
+                        System.out.println("Making move level \"medium\"");
+                        mediumMove(board, num % 2 == 0 ? "O" : "X");
+                        ++num;
+                        if (winCheck(board).equalsIgnoreCase("X")) {
+                            System.out.println("X wins");
                             break;
                         }
                         if (fillCheck(board)) {
@@ -347,10 +450,11 @@ public class tictactoe {
                             break;
                         }
                     } else {
-                        System.out.println("Making move level \"medium\"");
-                        mediumMove(board, num % 2 == 0 ? "O" : "X");
+                        System.out.println("Making move level \"hard\"");
+                        hardMove(board, num % 2 == 0 ? "O" : "X");
                         ++num;
-                        if (winCheck(board)) {
+                        if (winCheck(board).equalsIgnoreCase("X")) {
+                            System.out.println("X wins");
                             break;
                         }
                         if (fillCheck(board)) {
@@ -361,7 +465,8 @@ public class tictactoe {
                     if (parameter[2].equalsIgnoreCase("user")) {
                         playerMove(board, num % 2 == 0 ? "O" : "X");
                         ++num;
-                        if (winCheck(board)) {
+                        if (winCheck(board).equalsIgnoreCase("O")) {
+                            System.out.println("O wins");
                             break;
                         }
                         if (fillCheck(board)) {
@@ -372,7 +477,20 @@ public class tictactoe {
                         System.out.println("Making move level \"easy\"");
                         easyMove(board, num % 2 == 0 ? "O" : "X");
                         ++num;
-                        if (winCheck(board)) {
+                        if (winCheck(board).equalsIgnoreCase("O")) {
+                            System.out.println("O wins");
+                            break;
+                        }
+                        if (fillCheck(board)) {
+                            System.out.println("Draw");
+                            break;
+                        }
+                    } else if (parameter[2].equalsIgnoreCase("medium")){
+                        System.out.println("Making move level \"medium\"");
+                        mediumMove(board, num % 2 == 0 ? "O" : "X");
+                        ++num;
+                        if (winCheck(board).equalsIgnoreCase("O")) {
+                            System.out.println("O wins");
                             break;
                         }
                         if (fillCheck(board)) {
@@ -380,10 +498,11 @@ public class tictactoe {
                             break;
                         }
                     } else {
-                        System.out.println("Making move level \"medium\"");
-                        mediumMove(board, num % 2 == 0 ? "O" : "X");
+                        System.out.println("Making move level \"hard\"");
+                        hardMove(board, "O");
                         ++num;
-                        if (winCheck(board)) {
+                        if (winCheck(board).equalsIgnoreCase("O")) {
+                            System.out.println("O wins");
                             break;
                         }
                         if (fillCheck(board)) {
@@ -396,7 +515,5 @@ public class tictactoe {
                 System.out.println("Bad parameters!");
             }
         }
-        
-
     }
 }
